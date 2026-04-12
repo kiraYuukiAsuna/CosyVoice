af27f6b9dfab1a1ca8086303f4f06fe45514599f
kiraYuukiAsuna <kirayuukiasuna@outlook.com>
2026-04-13 04:13:26 +0800

    Update

--- Modified/Finetune/cosyvoice3/Script/04_train.sh
+++ Modified/Finetune/cosyvoice3/Script/04_train.sh
@@ -72,7 +72,6 @@ for idx in "${!MODELS[@]}"; do
     --ddp.dist_backend "$dist_backend" \
     --num_workers "$num_workers" \
     --prefetch "$prefetch" \
-    --pin_memory \
     --use_amp \
     --deepspeed_config ./conf/ds_stage2.json \
     --deepspeed.save_states model+optimizer
